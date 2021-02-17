f003ee1f19f473faabb2de8b69c485c884f9fa5e
JeongYounHan <51sarang@hanmail.net>
2021-02-18 01:22:32 +0900

    using slot

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="user-container">
+    <div class="user-description">
+      <router-link :to="`/user/${userInfo.id}`">
+        <slot name="username"></slot>
+      </router-link>
+      <div class="time">
+        <slot name="time"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    propsdata: Object
+  },
+  computed: {
+    userInfo() {
+      return this.$store.state.user;
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
-    <p>name : {{ userInfo.id }}</p>
-    <p>karma : {{ userInfo.karma }}</p>
-    <p>created : {{ userInfo.created }}</p>
+    <user-profile :propsdata="userInfo">
+      <div slot="username">{{ userInfo.id }}</div>
+      <template slot="time">{{ userInfo.created }}</template>
+    </user-profile>    
   </div>
 </template>
 
 <script>
+import UserProfile from '../components/UserProfile.vue'
+
 export default {
+  components: {
+    UserProfile,
+  },
   computed: {
     userInfo() {
       return this.$store.state.user;
