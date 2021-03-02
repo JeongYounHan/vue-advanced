3571f0fab388b071b6bc80ca5b78ff7ac8731eae
JeongYounHan <51sarang@hanmail.net>
2021-03-03 02:37:37 +0900

    라우터 가드

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,8 @@ import JobsView from '../views/JobsView.vue';
 import UserView from '../views/UserView.vue';
 import ItemView from '../views/ItemView.vue';
 // import createListView from '../views/CreateListView.js';
-
+import bus from '../utils/bus.js';
+import { store } from '../store/index.js'
 
 Vue.use(VueRouter)
 
@@ -22,18 +23,43 @@ const router = new VueRouter({
       name: 'news',
       component: NewsView,
       // component: createListView('NewsView'),
+      beforeEnter: (to, from, next) => {
+        bus.$emit('start:spinner');
+        store.dispatch('FETCH_LIST', to.name)
+          .then(() => {
+            // bus.$emit('end:spinner');
+            next();
+          })
+          .catch((err) => console.log(err));     
+      }
     },
     {
       path: '/ask',
       name: 'ask',
       component: AskView,
       // component: createListView('AskView'),
+      beforeEnter: (to, from, next) => {
+        bus.$emit('start:spinner');
+        store.dispatch('FETCH_LIST', to.name)
+          .then(() => {
+            next();
+          })
+          .catch((err) => console.log(err));     
+      }
     },
     {
       path: '/jobs',
       name: 'jobs',
       component: JobsView,
       // component: createListView('JobsView'),
+      beforeEnter: (to, from, next) => {
+        bus.$emit('start:spinner');
+        store.dispatch('FETCH_LIST', to.name)
+          .then(() => {
+            next();
+          })
+          .catch((err) => console.log(err));     
+      }
     },
     {
       path: '/user/:id',
