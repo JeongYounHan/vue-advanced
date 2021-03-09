19f6f6bf07e238499c95599523a62d0a0eace2a6
JeongYounHan <51sarang@hanmail.net>
2021-03-10 02:12:56 +0900

    slot design pattern

---
+++ src/views/designPattern/Slot.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <ul>
+      <!-- 이건 기존의 방식 -->
+      <!-- <item v-for="item in items" :item="item"></item> -->
+
+      <!-- 아니 컴포넌트 중간에 글자가 왜`~~~? -->
+      <item>아이템1</item> 
+      <item>
+        아이템2 
+        <button>Click</button>
+      </item>
+      <item>
+        <div>
+          아이템3
+        </div>
+        <img src="" alt="">
+      </item>
+      <Button disabled>
+        아이템4
+      </Button>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Item from '../../components/slot/Item'
+import Button from '../../components/slot/Button'
+
+export default {
+  components: {
+    Item,
+    Button,
+  },
+}
+</script>
+
+<style>
+
+</style>
