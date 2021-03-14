540cc317c68808c4543e730462fd3453cb9105ce
JeongYounHan <51sarang@hanmail.net>
2021-03-14 20:50:58 +0900

    renderless design pattern

---
+++ src/components/renderless/FetchData.vue
@@ -0,0 +1,33 @@
+<script>
+import axios from 'axios';
+
+export default {
+  props: ['url'],
+  data() {
+    return {
+      response: null,
+      loading: true,
+    }
+  },
+  created() {
+    axios.get(this.url)
+      .then(res => {
+        this.response = res.data;
+        this.loading = false;
+      })
+      .catch(err => {
+        alert('error', err);
+      })
+  },
+  render() {
+    return this.$scopedSlots.default({
+      response: this.response,
+      loading: this.loading,
+    });
+  }
+}
+</script>
+
+<style>
+
+</style>
